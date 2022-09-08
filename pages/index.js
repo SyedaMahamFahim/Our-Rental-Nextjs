393b07c3cecc058640f26504431ba4710db5b3fb
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2022-09-09 02:37:29 +0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import Head from "next/head";
-import Image from "next/image";
 import BrandBox from "../components/BrandBox/BrandBox";
-
 import Heros from "../components/HeroSection/HeroSection";
 import HowItsWork from "../components/HowItsWork/HowItsWork";
 import FeatureGridListing from "../container/FeatureGridListing/FeatureGridListing";
 import LocationAdvantages from "../container/LocationAdvantages/LocationAdvantages";
 import Testimonials from "../container/Testimonials/Testimonials";
-// import FeatureListing from '../container/FeatureListing/FeatureListing'
 export default function Home() {
   return (
     <>
